a0ab403cb3b0c51b7c09ecc7b2e762e49a0e94a9
Ryuya Nishino <ryuyasamaaaa@gmail.com>
2019-02-13 15:47:53 +0900

    improve algorithm

--- index.js
+++ index.js
@@ -127,12 +127,12 @@ const lineUserId = process.env.LINE_USER_ID;
                         var num = await Math.floor(Math.random() * 3) + 1;
                         console.log(num);
 
-                        await page.waitFor(300);
+                        await page.waitFor(400);
 
-                        var choice_text = await page.$$('.bar .quiz_tit')[num].textContent;
-                        // var choice_text = await page.$$eval('.bar .quiz_tit', list => {
-                        //     return list[num].textContent;
-                        // });
+                        //var choice_text = await page.$$('.bar .quiz_tit')[num].textContent;
+                        var choice_text = await page.$$eval('.bar .quiz_tit', list => {
+                            return list[num].textContent;
+                        });
 
                         //var choice_text = await page.$$('.bar .quiz_tit')[num].textContent;
                         console.log(choice_text);
